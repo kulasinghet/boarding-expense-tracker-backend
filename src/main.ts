6daac72612d7cd9707c5799980dd1e48e5e5fb57
Thevindu <kulasinghet@gmail.com>
2023-02-19 20:26:38 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS
+  app.enableCors({
+    origin: '*', // Allow requests from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Set the allowed HTTP methods
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true, // Allow credentials such as cookies and authorization headers
+  });
+
   await app.listen(3000);
 }
 bootstrap();
